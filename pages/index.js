8fbd4753772bb875d722ba9a5ff9b091fe77c200
Johan De Coster <johan.decoster@etexgroup.com>
2023-04-12 21:09:36 +0200

    Add files

---
+++ pages/index.js
@@ -0,0 +1,441 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const NFTcontract="0x006c4237E2233fc5b3793aD9E200076C9Cf99a0E";
+const zillowurl='https://api.bridgedataoutput.com/api/v2/pub/transactions?access_token=d555ec24e3f182c86561b09d0a85c3dc&limit=1&sortBy=recordingDate&order=desc&fields=recordingDate,parcels.apn,parcels.full&documentType=grant&recordingDate.gt=2015-01-01&parcels.apn=';
+const myabi=[
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_realtor",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_sellbydays",
+				"type": "uint256"
+			}
+		],
+		"name": "createBonus",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "_realtor",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_startdate",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_sellbydays",
+				"type": "uint256"
+			}
+		],
+		"name": "createBonusTest",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "parcelid",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "lastrecordedDate",
+				"type": "uint256"
+			}
+		],
+		"name": "realtorwithdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "parcelid",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "lastrecordedDate",
+				"type": "uint256"
+			}
+		],
+		"name": "sellerwithdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonusActive",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonusamount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonusrealtor",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonussellbydate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonusseller",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_parcelid",
+				"type": "string"
+			}
+		],
+		"name": "getBonusstartdate",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastapn",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastrequestid",
+		"outputs": [
+			{
+				"internalType": "bytes32",
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "lastselldate",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "parcelbonus",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "parcelid",
+				"type": "string"
+			},
+			{
+				"internalType": "address",
+				"name": "seller",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "realtor",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "startdate",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "sellbydate",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "active",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "parcellastselldate",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "url",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "urlresult",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+const {ethers} = require('ethers');
+var provider;
+var MyContract;
+var MyContractwSigner;
+
+const checkseller = async() => {
+	
+	var APN = '290-15-153';
+	
+	if (typeof window.ethereum !== 'undefined') {
+		console.log('MetaMask is installed!');
+		
+	}
+	console.log('Metamask enabled');
+	const accounts = await window.ethereum.send(
+    "eth_requestAccounts"
+  )
+  //console.log('accounts', accounts.result[0]);
+  const address = accounts.result[0];
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner(address);
+  //console.log(signer);
+  //var blockNumber = await provider.getBlockNumber();
+  //console.log('Block number = '+ blockNumber);
+  MyContract = new ethers.Contract(NFTcontract, myabi, provider);
+  //console.log(MyContract);
+  MyContractwSigner = await MyContract.connect(signer);
+	var result = await MyContract.getBonusseller(APN);
+	//document.getElementById("myresponse").value=result;
+  console.log(result);
+}
+
+export default function Home() {
+  return (
+    <div>
+    <p>Test</p>
+    <button onClick = {checkseller}>Test Button</button>
+    </div>
+  )
+}
